f8edc0398ffa6aa01cc6d957bd56c36b88796a9c
Joon Park <jsp220@gmail.com>
2022-09-25 21:42:08 -0700

    Create employee class and manager subclass; write quick script in index.js to test classes

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const employee = require("./lib/employee");
+const manager = require("./lib/manager");
 
+const joon = new employee ("Joon", 1, "a@b.com");
+const drew = new manager ("Drew", 2, "ab@cd.com", "987-654-3210");
+
+console.log(joon);
+console.log(drew);
+
+console.log(joon.getName());
+console.log(joon.getId());
+console.log(joon.getEmail());
+console.log(joon.getRole());
+
+console.log(drew.getName());
+console.log(drew.getId());
+console.log(drew.getEmail());
+console.log(drew.getRole());
