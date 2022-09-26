b132596ee66c45696d79e68c6768b4be17d32eff
Joon Park <jsp220@gmail.com>
2022-09-25 23:19:38 -0700

    Add functions in index.js to prompt for team members

--- index.js
+++ index.js
@@ -5,17 +5,159 @@ const manager = require("./lib/manager");
 const engineer = require("./lib/engineer");
 const intern = require("./lib/intern");
 
-// const joon = new employee ("Joon", 1, "a@b.com");
-// const joon = new manager ("Joon", 2, "ab@cd.com", "987-654-3210");
-// const joon = new engineer ("Joon", 2, "ab@cd.com", "jsp220");
-const joon = new intern ("Joon", 2, "ab@cd.com", "UCSD");
+let team = [];
 
-// console.log(joon);
+function init() {
+    console.clear();
+    
+    console.log(`\x1b[36m### Team Profile Generator ###\x1b[0m\n`)
+   
+    function addTeamMember () {
+        inquirer
+            .prompt([{
+                type: "list",
+                message: "Please select the type of employee you would like to add.",
+                name: "employeeType",
+                choices: ["Manager", "Engineer", "Intern", "I am done"],
+            }])
+            .then(data => {
+                switch(data.employeeType) {
+                    case "Manager":
+                        addManager();
+                        break;
+                    case "Engineer":
+                        addEngineer();
+                        break;
+                    case "Intern":
+                        addIntern();
+                        break;
+                    default:
+                        createHtml();
+                        break;
+                }
+            })
+    }
+    
+    function addManager () {
+        inquirer
+            .prompt ([
+                {
+                    type: "input",
+                    message: "Please enter the name of the manager.",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "Please enter the manager's employee ID number.",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "Please enter the manager's email address.",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "Please enter the manager's office number.",
+                    name: "officeNumber",
+                }
+            ]).then(data => {
+                const {name, id, email, officeNumber} = data;
+                const mgr = new manager(name, id, email, officeNumber);
+                team.push(mgr);
+                console.log(team);
+                addTeamPrompt();
+            })
+    }
 
-console.log(joon.getName());
-console.log(joon.getId());
-console.log(joon.getEmail());
-// console.log(joon.getOfficeNumber());
-// console.log(joon.getGithub());
-console.log(joon.getSchool());
-console.log(joon.getRole());
+    function addEngineer () {
+        inquirer
+            .prompt ([
+                {
+                    type: "input",
+                    message: "Please enter the name of the engineer.",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "Please enter the engineer's employee ID number.",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "Please enter the engineer's email address.",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "Please enter the engineer's GitHub username.",
+                    name: "github",
+                }
+            ]).then(data => {
+                const {name, id, email, github} = data;
+                const eng = new engineer(name, id, email, github);
+                team.push(eng);
+                console.log(team);
+                addTeamPrompt();
+            })
+    }
+
+    function addIntern () {
+        inquirer
+            .prompt ([
+                {
+                    type: "input",
+                    message: "Please enter the name of the intern.",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "Please enter the intern's employee ID number.",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "Please enter the intern's email address.",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "Please enter the name of the intern's school.",
+                    name: "school",
+                }
+            ]).then(data => {
+                const {name, id, email, school} = data;
+                const int = new intern(name, id, email, school);
+                team.push(int);
+                // console.log(team);
+                addTeamPrompt();
+            })
+    }
+
+    function addTeamPrompt() {
+        console.clear();
+        
+        console.log("So far you have added:");
+        for (let i in team) {
+            console.log(`${team[i].getRole()}: ${team[i].getName()}`);
+        }
+        inquirer
+            .prompt([{
+                type: "list",
+                message: "Would you like to add more team members?",
+                name: "addMore",
+                choices: ["Yes", "No"],
+            }]).then(data => {
+                if (data.addMore == "Yes") {
+                    addTeamMember();
+                } else {
+                    createHtml();
+                };
+            });
+    }
+        
+    addTeamMember();
+}
+
+
+init();
