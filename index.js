ee5027ab81354ad8155ee90a4babe22297abdbdf
Joon Park <jsp220@gmail.com>
2022-09-25 22:00:43 -0700

    Add engineer and intern classes

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const employee = require("./lib/employee");
 const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
 
-const joon = new employee ("Joon", 1, "a@b.com");
-const drew = new manager ("Drew", 2, "ab@cd.com", "987-654-3210");
+// const joon = new employee ("Joon", 1, "a@b.com");
+// const joon = new manager ("Joon", 2, "ab@cd.com", "987-654-3210");
+// const joon = new engineer ("Joon", 2, "ab@cd.com", "jsp220");
+const joon = new intern ("Joon", 2, "ab@cd.com", "UCSD");
 
-console.log(joon);
-console.log(drew);
+// console.log(joon);
 
 console.log(joon.getName());
 console.log(joon.getId());
 console.log(joon.getEmail());
+// console.log(joon.getOfficeNumber());
+// console.log(joon.getGithub());
+console.log(joon.getSchool());
 console.log(joon.getRole());
-
-console.log(drew.getName());
-console.log(drew.getId());
-console.log(drew.getEmail());
-console.log(drew.getRole());
